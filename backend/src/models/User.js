4e2dd5447e3cb3d02009d8c9e22e0da9b8e27a4f
mayrars <mayra203@gmail.com>
2025-06-04 06:02:21 -0600

    created userschema for save and login

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -22,6 +23,19 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model("User", UserSchema);
+//hash password before saving user to db
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
