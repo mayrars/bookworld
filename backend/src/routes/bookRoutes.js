bffc0c90394206ae59b7c973c17cf520c9e22845
mayrars <mayra203@gmail.com>
2025-06-11 06:08:05 -0600

    correct get books and add delete books

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cloudinary from '../lib/cloudinary.js'
 import Book from '../models/book.js'
 import User from '../models/User.js'
+import protectRoute from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
@@ -21,7 +22,7 @@ router.post("/", protectRoute, async(req, res) => {
             rating, 
             image: imageUrl, 
             author,
-            //user: req.user._id,
+            user: req.user._id,
         })
 
         await newBook.save()
@@ -32,5 +33,58 @@ router.post("/", protectRoute, async(req, res) => {
     }
 })
 
+
+router.get("/", protectRoute, async(req, res) => {
+    try {
+        const page = req.query.page || 1
+        const limit = req.query.limit || 5
+        const skip = (page - 1) * limit
+
+        const books = await Book.find()
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate("user", "username profileImage")
+
+        const totalBooks = await Book.countDocuments()
+
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil(totalBooks / limit)
+        })
+    } catch (error) {
+        console.log(err)
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+router.delete("/:id", protectRoute, async(req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        if(!book) return res.status(404).json({ message: "Book not found" })
+        
+        if(book.user.toString() !== req.user._id.toString()) 
+            return res.status(401).json({ message: "You are not authorized to delete this book" })
+
+        if(book.image && book.image.includes("cloudinary")){
+            try{
+                const publicId = book.image.split("/").pop().split(".")[0]
+                await cloudinary.uploader.destroy(publicId)
+            }catch(deleteError){
+                console.log("Error deleting image from cloudinary", deleteError)
+            }
+        }
+
+        await book.deleteOne()
+
+        res.json({ message: "Book deleted successfully" })
+        
+    } catch (error) {
+        console.log("Error deleting book",error)
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
 
 export default router
