c424ea59365c8ce69ab5f228d7f435fab72e53d6
mayrars <mayra203@gmail.com>
2025-06-11 06:08:35 -0600

    protect router and add token

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -20,7 +20,16 @@ const protectRoute = async(req, res, next) => {
             return res.status(401).json({message: "Not authorized"})
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch(error){
 
+        const user = await User.findById(decoded.userId).select("-password")
+        if (!user) return res.status(401).json({message: "Token is not valid"})
+
+        req.user = user
+        next()
+    } catch(error){
+        console.error("Authentication error:", error.message)
+        res.status(401).json({message: "Token us not failed"})
     }
 }
+
+export default protectRoute
