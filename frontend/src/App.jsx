44e88c29f054c79b663b744f5004602a6970d8ef
mayrars <mayra203@gmail.com>
2025-06-20 05:33:57 -0600

    changed app delete contente and add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,19 @@
 import './App.css'
-import Slider from "react-slick";
-import Header from './components/Header';
-import image1 from './assets/cuaderno-de-mano-de-alto-angulo.jpg';
-import image2 from './assets/mujer-sonriente-de-vista-frontal-con-libro.jpg';
-import image3 from './assets/personas-de-alto-angulo-leyendo-juntas.jpg';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import Login from './pages/Login/Login';
+import Layout from './Layout';
 
 function App() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    responsive: [
-      {
-        breakpoint: 1800,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
+  
   return (
     <>
-      <Header />
-      <section class="bg-white text-black">
-        <div class="container py-0 px-0 mx-auto max-w-[1800px]">
-          <Slider {...settings}>
-            <div>
-              <img src={image1} alt="Logo" />
-            </div>
-            <div>
-              <img src={image2} alt="Logo" />
-            </div>
-            <div>
-              <img src={image3} alt="Logo" />
-            </div>
-          </Slider>
-          <div class="max-w-screen-lg">
-          </div>
-        </div>
-      </section>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
     </>
   )
 }
