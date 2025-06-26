aea8fd38da44c26348514688468974580f7952ca
mayrars <mayra203@gmail.com>
2025-06-26 06:06:15 -0600

    adding private routes, dashboard and private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Login from './pages/Login/Login';
 import Layout from './Layout';
+import ErrorPage from './pages/Error/ErrorPage';
+import Dashboard from './pages/Dashboard/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   
@@ -12,6 +16,11 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />}/>
           <Route path="/login" element={<Login />} />
+          {/* Rutas protegidas */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </>
