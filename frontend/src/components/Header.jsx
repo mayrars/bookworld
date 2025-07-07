77b3ed0427acfe44c4fa1ca9e7782b9e6426c22e
mayrars <mayra203@gmail.com>
2025-07-07 05:33:35 -0600

    agregando link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -47,17 +47,17 @@ const Header = () => {
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-              >
-                Home
-              </Link>
-            </li>
 
             {!isAuthenticated ? (
               <>
+                <li>
+                  <Link
+                    to="/"
+                    className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  >
+                    Home
+                  </Link>
+                </li>
                 <li>
                   <Link
                     to="/signup"
@@ -84,6 +84,9 @@ const Header = () => {
                   >
                     Dashboard
                   </Link>
+                </li>
+                <li>
+                  <Link to="/my-books" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700">Mis libros</Link>
                 </li>
                 <li>
                   <Link
